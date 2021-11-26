578f7e54a169a28e8c131e2dd2b384ca13f99848
LorenzoGirinelli <girinellilorenzo22@hotmail.com>
2021-11-26 16:28:03 +0100

    inizio snack 2

---
+++ snack 2/js/script.js
@@ -0,0 +1,38 @@
+const serieA =
+[
+    {
+        nome: 'Atalanta',
+        punti: 0,
+        falli: 0,
+    },
+    {
+        nome: 'Bologna',
+        punti: 0,
+        falli: 0,
+    },
+    {
+        nome: 'Hellas Verona',
+        punti: 0,
+        falli: 0,
+    },
+    {
+        nome: 'Lazio',
+        punti: 0,
+        falli: 0,
+    },
+    {
+        nome: 'Milan',
+        punti: 0,
+        falli: 0,
+    },
+    {
+        nome: 'Sampdoria',
+        punti: 0,
+        falli: 0,
+    },{
+        nome: 'Inter',
+        punti: 0,
+        falli: 0,
+    }
+
+]
