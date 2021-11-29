60ce07887901543a0b3363079fc95e16743188cd
LorenzoGirinelli <girinellilorenzo22@hotmail.com>
2021-11-29 10:18:25 +0100

    metà

--- snack 2/js/script.js
+++ snack 2/js/script.js
@@ -3,36 +3,51 @@ const serieA =
     {
         nome: 'Atalanta',
         punti: 0,
-        falli: 0,
+        falliSubiti: 0,
     },
     {
         nome: 'Bologna',
         punti: 0,
-        falli: 0,
+        falliSubiti: 0,
     },
     {
         nome: 'Hellas Verona',
         punti: 0,
-        falli: 0,
+        falliSubiti: 0,
     },
     {
         nome: 'Lazio',
         punti: 0,
-        falli: 0,
+        falliSubiti: 0,
     },
     {
         nome: 'Milan',
         punti: 0,
-        falli: 0,
+        falliSubiti: 0,
     },
     {
         nome: 'Sampdoria',
         punti: 0,
-        falli: 0,
+        falliSubiti: 0,
     },{
         nome: 'Inter',
         punti: 0,
-        falli: 0,
+        falliSubiti: 0,
     }
 
-]
+];
+for(let i = 0; i < serieA.length; i++) {
+    const thisteam = serieA[i];;
+
+    const randomFalliSubiti = getRndInteger(0, 100);
+    const randomPunti = getRndInteger(0, 100);
+
+    thisteam.punti= randomPunti;
+    thisteam.falliSubiti= randomFalliSubiti;
+    console.log(thisteam);
+}
+console.log(serieA);
+// FUNCTION
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
